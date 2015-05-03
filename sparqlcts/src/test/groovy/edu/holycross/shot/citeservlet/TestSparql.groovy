4eb355af3eec34caa6da4a02f574b92a357cd852
Neel Smith <dnsmith.neel@gmail.com>
2015-05-03 17:37:08 -0400

    Light tidying of API docs

--- sparqlcts/src/test/groovy/edu/holycross/shot/citeservlet/TestSparql.groovy
+++ sparqlcts/src/test/groovy/edu/holycross/shot/citeservlet/TestSparql.groovy
@@ -3,20 +3,17 @@ package edu.holycross.shot.citeservlet
 import static org.junit.Assert.*
 import org.junit.Test
   
-class TestSparql extends GroovyTestCase {
+class TestSparqlInteg extends GroovyTestCase {
 
-  // remember how to pass in property values to
-  // test case...?
   String baseUrl = "http://localhost:8080/fuseki/ds/query"
-  String verbs = "SELECT DISTINCT ?v WHERE {?s ?v ?o . } "
+  String verbQuery = "SELECT DISTINCT ?v WHERE {?s ?v ?o . } "
   Integer expectedNumberVerbs = 15
   
   @Test
   void testSetup() {
     Sparql sparql = new Sparql(baseUrl)
     def slurper = new groovy.json.JsonSlurper()
-    def parsedColls = slurper.parseText(sparql.getSparqlReply("application/json", verbs))
-    println "PARSED: " + parsedColls
+    def parsedColls = slurper.parseText(sparql.getSparqlReply("application/json", verbQuery))
     def verbList = []
     parsedColls.results.bindings.each { b ->
       verbList.add(b.v.value)
