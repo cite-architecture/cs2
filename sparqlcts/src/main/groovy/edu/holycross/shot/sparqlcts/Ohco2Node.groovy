d27fe8030bdc445a46da7677d7029d69f65735f5
Neel Smith <dnsmith.neel@gmail.com>
2015-05-05 12:52:20 -0400

    Add label to OHCO2 model

--- sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/Ohco2Node.groovy
+++ sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/Ohco2Node.groovy
@@ -8,24 +8,35 @@ import edu.harvard.chs.cite.CtsUrn
 class Ohco2Node {
 
 
-  /** URN for the node. */
+  /** Machine-actionable URN for the node. */
   CtsUrn nodeUrn
+  /** Human-readable label for the node*/
+  String nodeLabel
+  
   /** Possibly null URN for preceding leaf node. */
   CtsUrn prevUrn = null
   /** Possibly null URN for following leaf node. */
   CtsUrn nextUrn = null
   /** Text content of the node. */
   String textContent
+
+  
   
   /** Constructor requiring all member properties. 
    */
-  Ohco2Node(CtsUrn urn, CtsUrn prev, CtsUrn next, String txt)
+  Ohco2Node(CtsUrn urn, String label, CtsUrn prev, CtsUrn next, String txt)
   throws Exception {
     if (urn == null) {
       throw new Exception("Ohco2Node: URN for node cannot be null.")
     } else {
       this.nodeUrn = urn
     }
+    if ((label == null) || (label.size() < 1)) {
+      throw new Exception("Ocho2Node: text content of node cannot be null.")
+    } else {
+      this.nodeLabel = label
+    }
+    
     if ((txt == null) || (txt.size() < 1)) {
       throw new Exception("Ocho2Node: text content of node cannot be null.")
     } else {
--- sparqlcts/src/test/groovy/edu/holycross/shot/sparqlcts/TestOhco2.groovy
+++ sparqlcts/src/test/groovy/edu/holycross/shot/sparqlcts/TestOhco2.groovy
@@ -8,23 +8,21 @@ import edu.harvard.chs.cite.CtsUrn
 
 class TestOhco2 extends GroovyTestCase {
 
-
   CtsUrn firstNode = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001:1.1")
   CtsUrn secondNode = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001:1.2")
   CtsUrn thirdNode = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001:1.3")
   
   @Test
   void testConstructor() {
-    Ohco2Node o2node = new Ohco2Node(secondNode, firstNode, thirdNode, "οὐλομένην κτλ")
+    Ohco2Node o2node = new Ohco2Node(secondNode, "Iliad 1.2", firstNode, thirdNode, "οὐλομένην κτλ")
     assert o2node
 
-    assert shouldFail {
-      Ohco2Node nullText = new Ohco2Node(secondNode, firstNode, thirdNode, "")
-    }
-
-
-    Ohco2Node emptyPrev = new Ohco2Node(firstNode, null, secondNode,"μῆνιν κτλ")
+    Ohco2Node emptyPrev = new Ohco2Node(firstNode, "Iliad 1.1.",  null, secondNode,"μῆνιν κτλ")
     assert emptyPrev
+
+    assert shouldFail {
+      Ohco2Node nullText = new Ohco2Node(secondNode, "Node with no text -- very, very bad!", firstNode, thirdNode, "")
+    }
   }
   
 }
