35af7b15ea846eb2675ab6b104b0ca4055a630ab
Neel Smith <dnsmith.neel@gmail.com>
2015-05-05 08:30:39 -0400

    Add working unit test

--- sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/Cts.groovy
+++ sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/Cts.groovy
@@ -2,19 +2,28 @@ package edu.holycross.shot.sparqlcts
 
 import edu.holycross.shot.citeservlet.Sparql
 
+//import edu.harvard.chs.cite.CiteUrn
+import edu.harvard.chs.cite.CtsUrn
+
 
 
 /** A class representing a Canonical Text Service.
  */
 class Cts {
 
+  /** SPARQL endpoint class from citeservlet common. */
   Sparql sparql
 
   /** Constructor requires base URL for SPARQL endpoint.
    */
   Cts(Sparql sparqlService) {
     this.sparql = sparqlService
+  }
 
+
+  
+
+  void getPrevNextReply(CtsUrn urn) {
   }
  
 }
---
+++ sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/CtsGraph.groovy
@@ -0,0 +1,16 @@
+package edu.holycross.shot.sparqlcts
+
+import edu.holycross.shot.citeservlet.Sparql
+
+
+
+/** A class representing a Canonical Text Service.
+ */
+class CtsGraph {
+
+
+  CtsGraph() {
+  }
+
+ 
+}
