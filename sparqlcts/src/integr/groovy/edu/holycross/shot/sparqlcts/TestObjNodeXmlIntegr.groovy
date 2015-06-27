6824f4666f7983c6b0deedfa42f7cc740aee9c54
Neel Smith <dnsmith.neel@gmail.com>
2015-06-27 09:56:35 -0400

    Add integr. test

---
+++ sparqlcts/src/integr/groovy/edu/holycross/shot/sparqlcts/TestObjNodeXmlIntegr.groovy
@@ -0,0 +1,28 @@
+package edu.holycross.shot.sparqlcts
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+
+import edu.harvard.chs.cite.CtsUrn
+import edu.holycross.shot.citeservlet.Sparql
+import edu.holycross.shot.sparqlcts.CtsGraph
+
+
+class TestObjNodeXmlIntegr extends GroovyTestCase {
+
+  String baseUrl = "http://localhost:8080/fuseki/ds/query"
+  Sparql sparql = new Sparql(baseUrl)
+  CtsGraph graph = new CtsGraph(sparql)
+
+  CtsUrn urn = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:1.1")
+  
+  @Test
+  void testLeafObjectXML() {
+    Ohco2Node onode = graph.getLeafNodeObject(urn)
+    println "XML IS: " + onode.toXml()
+  }
+
+
+  
+}
