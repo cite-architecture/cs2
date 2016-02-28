382c6e7bf1190e510e3f287c6db2b9c5830c158a
Christopher Blackwell <cwblackwell@gmail.com>
2016-02-28 08:24:00 -0500

    moved all xml formatting stuff to XmlFormatter.

--- sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/CtsReply.groovy
+++ sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/CtsReply.groovy
@@ -61,87 +61,18 @@ class CtsReply {
 	 * @returns ctsReply as Map
 	 */
 	String getPassagePlusToXML(CtsUrn requestUrn){
-		String xmlString = "holding"
+        StringBuffer xmlString = new StringBuffer()
+		String xmlCtsFragString = ""
 			Map gppObject = getPassagePlusReply(requestUrn)
-			xmlString = getFillText(gppObject['GetPassagePlus']['reply']['rangeNodesMap'])
+			
+			xmlCtsFragString = formatter.buildXmlFragment(gppObject['GetPassagePlus']['reply']['rangeNodesMap'])
+			// Format for CTS Reply
+			xmlString.append(xmlCtsFragString)
 			return xmlString
 	}
 
-	/**  Crazy complicated invokating on XmlFormatter, to 
-	* ensure a well-formed XML fragment. Highly implementation-specific.
-	* Fragile as hell. Don't mess withis lightly.
-	 * @returns String
-	 */
-	String getFillText(ArrayList leafNodes){
-        StringBuffer passageString = new StringBuffer()
 
-		// Check for XML
-		Boolean properXML = true
-		leafNodes.each{ b ->
-			properXML = (properXML && (b['typeExtras']['type'] == 'xml'))
-			properXML = (properXML && (b['typeExtras']['anc']))
-			properXML = (properXML && (b['typeExtras']['nxt']))
-			properXML = (properXML && (b['typeExtras']['xpt']))
-		}
-
-		if (properXML){
-
-		//	String currentWrapper = leafNodes[0]['typeExtras']['anc']
-			String currentWrapper = leafNodes[0]['typeExtras']['anc']
-			// We need to grab this, because when texts have different sections with different depths,
-			// we can't count on the next section having the same structure as the previous one.
-			String currentXpt = "" 
-			String currentNext = ""
-			def citeDiffLevel = 0
-			String tempText = ""
-			Boolean firstNode = true
-
-			leafNodes.each { b ->
-
-				if (b['typeExtras']['nxt'] != currentNext){
-					
-					currentNext = b['typeExtras']['nxt']
-					if ((b['typeExtras']['anc'] != currentWrapper)||(firstNode)) {
-						citeDiffLevel = formatter.findDifferingCitationLevel(b['typeExtras']['anc'], currentWrapper, b['typeExtras']['xpt'])
-						if (firstNode) {
-								firstNode = false
-								passageString.append(formatter.openAncestors(b['typeExtras']['anc'],b['typeExtras']['xmlns'],b['typeExtras']['xmlnsabbr']))
-						} else  {
-								if (citeDiffLevel < 0){
-								passageString.append(formatter.trimClose(b['typeExtras']['anc'], currentXpt,1))
-								passageString.append(formatter.trimAncestors(b['typeExtras']['anc'], b['typeExtras']['xpt'], 1))
-								} else {
-								// We might need to change 'b.xpt?.value' in the line below to 'currentXpt'
-								passageString.append(formatter.trimClose(b['typeExtras']['anc'], b['typeExtras']['xpt'],citeDiffLevel))
-								passageString.append(formatter.trimAncestors(b['typeExtras']['anc'], b['typeExtras']['xpt'], citeDiffLevel))
-								}
-						}
-						currentWrapper = b['typeExtras']['anc']
-						currentXpt = b['typeExtras']['xpt']
-					}
-                    if (b['rangeNode']['textContent']) {
-						//Here we are going to wrap the leaf-node in an element, 
-						//with the URN as an attribute
-						tempText = """<cts:node urn="${b['rangeNode']['nodeUrn']}">${b['rangeNode']['textContent']}</cts:node>"""
-						
-						//passageString.append(b.txt?.value)
-						passageString.append(tempText)
-                    }
-				}
-			}
-				passageString.append(formatter.closeAncestors(currentWrapper))
-
-		} else {
-			leafNodes.each { b ->
-				passageString.append("${b['rangeNode']['textContent']}\n\n")
-			}
-		}
-
-		
 	
-		return passageString	
-
-    }
 
   
 }
--- sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/XmlFormatter.groovy
+++ sparqlcts/src/main/groovy/edu/holycross/shot/sparqlcts/XmlFormatter.groovy
@@ -220,5 +220,79 @@ class XmlFormatter {
     }
     return formatted.toString()
   }
+
+	/**  Crazy complicated invokating on XmlFormatter, to 
+	* ensure a well-formed XML fragment. Highly implementation-specific.
+	* Fragile as hell. Don't mess withis lightly.
+	 * @returns String
+	 */
+	String buildXmlFragment(ArrayList leafNodes){
+        StringBuffer passageString = new StringBuffer()
+
+		// Check for XML
+		Boolean properXML = true
+		leafNodes.each{ b ->
+			properXML = (properXML && (b['typeExtras']['type'] == 'xml'))
+			properXML = (properXML && (b['typeExtras']['anc']))
+			properXML = (properXML && (b['typeExtras']['nxt']))
+			properXML = (properXML && (b['typeExtras']['xpt']))
+		}
+
+		if (properXML){
+
+		//	String currentWrapper = leafNodes[0]['typeExtras']['anc']
+			String currentWrapper = leafNodes[0]['typeExtras']['anc']
+			// We need to grab this, because when texts have different sections with different depths,
+			// we can't count on the next section having the same structure as the previous one.
+			String currentXpt = "" 
+			String currentNext = ""
+			def citeDiffLevel = 0
+			String tempText = ""
+			Boolean firstNode = true
+
+			leafNodes.each { b ->
+
+				if (b['typeExtras']['nxt'] != currentNext){
+					
+					currentNext = b['typeExtras']['nxt']
+					if ((b['typeExtras']['anc'] != currentWrapper)||(firstNode)) {
+						citeDiffLevel = findDifferingCitationLevel(b['typeExtras']['anc'], currentWrapper, b['typeExtras']['xpt'])
+						if (firstNode) {
+								firstNode = false
+								passageString.append(openAncestors(b['typeExtras']['anc'],b['typeExtras']['xmlns'],b['typeExtras']['xmlnsabbr']))
+						} else  {
+								if (citeDiffLevel < 0){
+								passageString.append(trimClose(b['typeExtras']['anc'], currentXpt,1))
+								passageString.append(trimAncestors(b['typeExtras']['anc'], b['typeExtras']['xpt'], 1))
+								} else {
+								// We might need to change 'b.xpt?.value' in the line below to 'currentXpt'
+								passageString.append(trimClose(b['typeExtras']['anc'], b['typeExtras']['xpt'],citeDiffLevel))
+								passageString.append(trimAncestors(b['typeExtras']['anc'], b['typeExtras']['xpt'], citeDiffLevel))
+								}
+						}
+						currentWrapper = b['typeExtras']['anc']
+						currentXpt = b['typeExtras']['xpt']
+					}
+                    if (b['rangeNode']['textContent']) {
+						//Here we are going to wrap the leaf-node in an element, 
+						//with the URN as an attribute
+						tempText = """<cts:node urn="${b['rangeNode']['nodeUrn']}">${b['rangeNode']['textContent']}</cts:node>"""
+						
+						//passageString.append(b.txt?.value)
+						passageString.append(tempText)
+                    }
+				}
+			}
+				passageString.append(closeAncestors(currentWrapper))
+
+		} else {
+			leafNodes.each { b ->
+				passageString.append("${b['rangeNode']['textContent']}\n\n")
+			}
+		}
+	
+		return passageString	
+
+    }
   
 }
