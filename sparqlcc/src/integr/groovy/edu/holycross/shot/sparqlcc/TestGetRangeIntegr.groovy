0650eb4fd43f7fd689856c108836b40da5a55205
Christopher Blackwell <cwblackwell@gmail.com>
2016-08-27 12:13:00 -0400

    Passing several tests on GetRange().

--- sparqlcc/src/integr/groovy/edu/holycross/shot/sparqlcc/TestGetRangeIntegr.groovy
+++ sparqlcc/src/integr/groovy/edu/holycross/shot/sparqlcc/TestGetRangeIntegr.groovy
@@ -28,20 +28,38 @@ class TestGetRangeIntegr extends GroovyTestCase {
     // Unordered collection, should get just first- and last-identified object
     CiteUrn urn = new CiteUrn("urn:cite:hmt:pageroi.3.v1-24.v1")
     CCOSet ccos = cc.getRange(urn)
-    assert ccos
+    assert ccos.urn.toString() == "urn:cite:hmt:pageroi.3.v1-24.v1"
+    assert ccos.startUrn.toString() == "urn:cite:hmt:pageroi.3.v1"
+    assert ccos.endUrn.toString() == "urn:cite:hmt:pageroi.24.v1"
+    assert ccos.ccos.size() == 2
   }
 
   @Test
   void testRange2(){
+    Sparql sparql = new Sparql(baseUrl)
+	  CcGraph cc = new CcGraph(sparql)
+
     // Ordered collection, should include 10 objects
     CiteUrn urn = new CiteUrn("urn:cite:hmt:venAsign.2601.v1-2610.v1")
+    CCOSet ccos = cc.getRange(urn)
+    assert ccos.urn.toString() == "urn:cite:hmt:venAsign.2601.v1-2610.v1"
+    assert ccos.startUrn.toString() == "urn:cite:hmt:venAsign.2601.v1"
+    assert ccos.endUrn.toString() == "urn:cite:hmt:venAsign.2610.v1"
+    assert ccos.ccos.size() == 10
 
   }
 
   @Test
   void testBadRange1(){
+    Sparql sparql = new Sparql(baseUrl)
+	  CcGraph cc = new CcGraph(sparql)
+
     // With an Ordered collection, you can't do this
     CiteUrn urn = new CiteUrn("urn:cite:hmt:venAsign.2610.v1-2601.v1")
+    shouldFail {
+      CCOSet ccos = cc.getRange(urn)
+      assert ccos
+    }
   }
 
 
