962f5276f686406a67a9be2fbfce738979541f1f
Neel Smith <dnsmith.neel@gmail.com>
2017-01-20 11:28:50 -0500

    Clean up CTS config file

---
+++ customcts/src/main/webapp/README.groovy
@@ -0,0 +1,26 @@
+
+html.html {
+	head {
+		title("CTS overlays")
+			link(href : "cts-ui/css/cts-core.css", rel : "stylesheet", title : "CSS for CTS", type : "text/css")
+
+	}
+
+	body {
+
+		header("SPARQL CTS")
+			nav {
+
+				p {
+					a(href : "http://cite-architecture.github.io", "Canonical Text Services")
+				}
+			}
+
+		article {
+      p("Badge your site up by dropping anything you want in this directory.")
+
+
+
+		}
+	}
+}
